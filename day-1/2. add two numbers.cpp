59ac459bf363cb051a97cd275320a49afbb7dea0
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-05-10 21:52:34 +0000

    program to add two numbers

---
+++ day-1/2. add two numbers.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+	std::cout<< "Enter two numbers: " <<std::endl;
+	int n1, n2, s;
+	std::cin >> n1 >> n2;
+	s = n1 + n2;
+	std::cout<< "The sum of "<<n1<<" and "<<n2<<" is "<<s<<std::endl;
+}
