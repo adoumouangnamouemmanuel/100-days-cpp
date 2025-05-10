b61a41637d8fa2df223753c8ebc6e2a917012936
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-05-10 22:01:38 +0000

    area of a circle

---
+++ day-1/5. area-of-a-circle.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	// calculate area of circle
+	cout<<"Enter the radius: "<<endl;
+	int r=0;
+	float a;
+	cin>>r;
+	a = 3.14 * r * r;
+	cout<<"The area of the circle of radius "<<r<<" is "<<a<<endl;
+}
