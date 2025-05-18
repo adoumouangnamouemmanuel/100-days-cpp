d2a65e71c9fe58ab7ee9d1cc0c031d7eed671b84
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-05-18 20:20:39 +0000

    area and peremeter of a rectangle

--- day-2/10. area-peremeter-rectangle.cpp
+++ day-2/10. area-peremeter-rectangle.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+	double L, W, A, P;
+	cout<<"Input the length of the rectangle : ";
+	cin>>L;
+	cout<<"Input the width of the rectangle : ";
+	cin>>W;
+	A = L*W;
+	P = 2 * (L + W);
+	cout<<"The area of the rectangle is : "<<A<<endl;
+	cout<<"The perimeter of the rectangle is : "<<P<<endl;
+}
