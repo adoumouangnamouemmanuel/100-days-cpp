fb2c84936bfcc07ff9965873781d804fe662e988
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-05-18 19:40:10 +0000

    short comments and docstring

--- day-2/5. compound-interest.cpp
+++ day-2/5. compound-interest.cpp
@@ -1,18 +1,29 @@
-#include <iostream> // Include input-output stream library
-#include <math.h>
+#include <iostream> // For input and output
+#include <math.h>   // For power function
 
 using namespace std;
 
-int main(){
-	double r, p, t, CI, A;
-	cout<<"Principal: ";
-	cin>>p;
-	cout<<"Rate: ";
-	cin>>r;
-	cout<<"Time: ";
-	cin>>t;
-	A = p * (pow((1+ r/100), t));
-	CI = A - p;
-	cout<<"Compound interest is: "<<CI<<endl;	
-	return 0;
+/**
+ * Calculates compound interest given principal, rate, and time.
+ * Formula: A = P(1 + r/100)^t, CI = A - P
+ */
+int main() {
+    double r, p, t, CI, A; // r: rate, p: principal, t: time, A: amount, CI: compound interest
+
+    cout << "Principal: ";
+    cin >> p;
+
+    cout << "Rate: ";
+    cin >> r;
+
+    cout << "Time: ";
+    cin >> t;
+
+    A = p * pow((1 + r / 100), t); // Final amount
+    CI = A - p;                    // Compound interest
+
+    cout << "Compound interest is: " << CI << endl;
+
+    return 0;
 }
+
