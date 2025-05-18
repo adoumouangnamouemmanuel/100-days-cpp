bfc605ca9551f04c59467debefcd4ac9969e7e81
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-05-18 19:31:51 +0000

    added comments and docstring

--- day-2/2. swap-two-numbers.cpp
+++ day-2/2. swap-two-numbers.cpp
@@ -1,16 +1,29 @@
-#include <iostream>
+/**
+ * Program to swap the values of two integers using a temporary variable.
+ * It takes two numbers as input and outputs their values before and after swapping.
+ */
+
+#include <iostream> // Include input-output stream library
 
 using namespace std;
 
-int main(){
-	int n1, n2, temp;
-	cout<<"Enter two numbers: "<<endl;
-	cin >>n1>>n2;
-	cout<<"Before: n1 = "<<n1<<". n2 = "<<n2<<endl;
-	
-	temp = n1;
-	n1 = n2;
-	n2 = temp;
-	cout<<"After: n1 = "<<n1<<". n2 = "<<n2<<endl;
-	
+int main() {
+    int n1, n2, temp; // Declare variables for two numbers and a temporary variable
+
+    cout << "Enter two numbers: " << endl; // Prompt user to input two numbers
+    cin >> n1 >> n2; // Read values into n1 and n2
+
+    // Display values before swapping
+    cout << "Before: n1 = " << n1 << ". n2 = " << n2 << endl;
+
+    // Swap values using a temporary variable
+    temp = n1;
+    n1 = n2;
+    n2 = temp;
+
+    // Display values after swapping
+    cout << "After: n1 = " << n1 << ". n2 = " << n2 << endl;
+
+    return 0; // Indicate successful program termination
 }
+
