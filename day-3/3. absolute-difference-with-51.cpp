2b5b16a3115bcc61f0ce07ba3703354bbe8d3421
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-05-19 22:52:52 +0000

    docstring for absolute difference

--- day-3/3. absolute-difference-with-51.cpp
+++ day-3/3. absolute-difference-with-51.cpp
@@ -1,18 +1,30 @@
+/**
+ * Program to compare a user-entered number with 51.
+ * If the number is greater than 51, it outputs triple the difference (n - 51).
+ * Otherwise, it outputs the absolute difference (51 - n).
+ */
+
 #include <iostream>
 
 using namespace std;
 
-int main(){
-	int num = 51;
-	int n;
-	cout<<"Enter a number: ";
-	cin>>n;
-	
-	if(n > num){
-		cout<<"("<<n<<" - "<<num<<")"<<" * 3"<<" = "<<(n - num)*3<<endl;
-	}
-	else{
-		 cout << num << " - " << n << " = " << num - n << endl;
-	}
-	return 0;
+int main() {
+    int num = 51;  // Reference number to compare against
+    int n;
+
+    // Prompt user to enter a number
+    cout << "Enter a number: ";
+    cin >> n;
+
+    // If input is greater than 51, print triple the difference
+    if (n > num) {
+        cout << "(" << n << " - " << num << ") * 3 = " << (n - num) * 3 << endl;
+    } 
+    else {
+        // Otherwise, print the absolute difference
+        cout << num << " - " << n << " = " << num - n << endl;
+    }
+
+    return 0;
 }
+
