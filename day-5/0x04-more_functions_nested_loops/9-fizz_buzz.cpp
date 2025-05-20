f4c1ab6f58b0655502f9a0a4b33cdbf1a2f024db
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-05-20 17:58:02 +0000

    fizz buzz

--- day-5/0x04-more_functions_nested_loops/9-fizz_buzz.cpp
+++ day-5/0x04-more_functions_nested_loops/9-fizz_buzz.cpp
@@ -2,39 +2,44 @@
 
 using namespace std;
 
-void fizzBuzz(void);
-
-void print_square(int size);  // Function prototype
+void fizzBuzz(void);  // Function prototype
 
 /**
- * main - Tests the print_square function with different sizes.
+ * main - Runs the fizzBuzz function.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-   fizzBuzz();
+    fizzBuzz();
     return 0;
 }
 
-// last character shoud not have a space after it
+/**
+ * fizzBuzz - Prints numbers from 1 to 100.
+ * For multiples of 3, prints "Fizz".
+ * For multiples of 5, prints "Buzz".
+ * For multiples of both 3 and 5, prints "FizzBuzz".
+ * Numbers and words are separated by a space,
+ * but the last output is not followed by a space.
+ */
+void fizzBuzz(void)
+{
+    for (int i = 1; i <= 100; i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+            cout << "FizzBuzz";
+        else if (i % 3 == 0)
+            cout << "Fizz";
+        else if (i % 5 == 0)
+            cout << "Buzz";
+        else
+            cout << i;
 
-
-void fizzBuzz(void){
-	int i;
-	for(i = 1; i <= 100; i++){
-		if( i % 3 == 0 && i % 5 == 0){
-			cout<<"FizzBuzz ";
-		}
-		else if( i % 3 == 0){
-			cout<<"Fizz ";
-		}
-		else if(i % 5 == 0){
-			cout<<"Buzz ";
-		}
-		else{
-			cout<<i<<" ";
-		}
-	}
-	cout<<endl;
+        // Print space after all except the last output
+        if (i != 100)
+            cout << " ";
+    }
+    cout << endl;
 }
+
