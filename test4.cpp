92b5b5d1432d52b4e8da1fae8cf9bce743849b9b
adoumouangnamouemmanuel <adoumouangnamouemmanuel@gmail.com>
2025-05-29 22:08:44 +0000

    test 4

--- test4.cpp
+++ test4.cpp
@@ -1,44 +1,17 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
-void printNumber(int *num){
-	cout<<*num<<endl;
-}
-
-void printLetter(char *num){
-	cout<<*num<<endl;
-}
-
-int getMin(int num[], int size){
-	int min = num[0];
-	int i;
-	for(i=0; i<size; i++){
-		if(num[i] < min)
-			min = num[i];
-	}
-	
-	return min;
-}
-
-int getMax(int num[], int size){
-	int max = num[0];
-	int i;
-	for(i=0; i<size; i++){
-		if(num[i] > max)
-			max = num[i];
-	}
-	
-	return max;
-}
-
 int main()
 {
-	int nums[5] = {5, 4, -3, 29, 6};
-	int min = getMin(nums, 5);
-	int max = getMax(nums, 5);
-	cout<<min<<endl;
-	cout<<max<<endl;
+	int x;
+	srand(time(0));
+	for(x=1; x<25; x++){
+		cout<<1+rand()%6<<endl;
+	}
+	
 //	system("pause>0");
 	
 	return 0;
